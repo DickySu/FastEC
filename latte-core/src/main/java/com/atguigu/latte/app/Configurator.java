b7456cc48346fcd4584dfb636116026ddf28b865
DickySu <929421352@qq.com>
2018-04-01 15:53:35 +0800

    初始化模式 单例 配置

--- latte-core/src/main/java/com/atguigu/latte/app/Configurator.java
+++ latte-core/src/main/java/com/atguigu/latte/app/Configurator.java
@@ -25,4 +25,30 @@ public class Configurator {
         LATTE_CONFIGS.put(ConfigKeys.CONFIG_READY, true);
     }
 
+    final WeakHashMap<Object, Object> getLatteConfigs() {
+        return LATTE_CONFIGS;
+    }
+
+    public final Configurator withApiHost(String host) {
+        LATTE_CONFIGS.put(ConfigKeys.API_HOST, host);
+        return this;
+    }
+
+    private void checkConfiguration() {
+        final boolean isReady = (boolean) LATTE_CONFIGS.get(ConfigKeys.CONFIG_READY);
+        if (!isReady) {
+            throw new RuntimeException("Configuration is not ready,call configure");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    final <T> T getConfiguration(Object key) {
+        checkConfiguration();
+        final Object value = LATTE_CONFIGS.get(key);
+        if (value == null) {
+            throw new NullPointerException(key.toString() + " IS NULL");
+        }
+        return (T) LATTE_CONFIGS.get(key);
+    }
 }
+
