be30a3c141f385dee29dd0d3ef7acab48296dc83
DickySu <929421352@qq.com>
2018-05-09 12:16:59 +0800

    主框架界面搭建 现在会显示空白 代码修改一点点

--- latte-core/src/main/java/com/atguigu/latte/delegates/LatteDelegate.java
+++ latte-core/src/main/java/com/atguigu/latte/delegates/LatteDelegate.java
@@ -1,8 +1,5 @@
 package com.atguigu.latte.delegates;
 
-/**
- * Created by 傅令杰 on 2017/4/2
- */
 
 public abstract class LatteDelegate extends PermissionCheckerDelegate {
 
--- latte-ec/src/main/java/com/atguigu/latteec/ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/atguigu/latteec/ec/main/index/IndexDelegate.java
@@ -3,20 +3,38 @@ package com.atguigu.latteec.ec.main.index;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.atguigu.latte.delegates.bottom.BottomItemDelegate;
 import com.atguigu.latteec.ec.R;
+import com.atguigu.latteec.ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
 
 /**
  * Created by su on 2018/5/8.
  */
 public class IndexDelegate extends BottomItemDelegate { //内容fragment
 
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
 
     @Override
     public Object setLayout() {
-        return R.layout.delegate_sort;
+        return R.layout.delegate_index;
     }
 
     @Override
